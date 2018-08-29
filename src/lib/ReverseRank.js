e792e4d0f5aa393e59560e3fabb2b22c51b2b885
Anne Bione <annebione@gmail.com>
2018-08-29 19:21:36 -0300

    Reverse rank function done

---
+++ src/lib/ReverseRank.js
@@ -0,0 +1,27 @@
+
+
+
+export default function ReverseRank(array) {
+    try {
+        if (Array.isArray(array)) {
+            if (arguments[1]) {
+                for (var i = 0; i < array.length; i ++) {
+                    if (typeof array[i][arguments[1]] == 'number') {
+                        return array[i][arguments[1]].sort(function(a, b) {
+                            return a[i][arguments[1]] < b[i][arguments[1]]
+                        }).reverse()    
+                    }
+                    if (typeof array[i][arguments[1]] == 'string') {
+                        return array[i][arguments[1]].sort().reverse()
+                    }
+                } 
+            } else {
+                array.sort().reverse();
+            }
+        } else {
+            throw new TypeError('Soul.Rank: ' + typeof(array) + ' is not an Array')
+        }
+    } catch(e) {
+        console.error(e);
+    }
+}
