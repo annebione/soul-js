b9c5678849a8e4a41f299b5515d9804aa49f789b
Anne Bione <annebione@gmail.com>
2018-08-29 19:36:10 -0300

    Initial setup

---
+++ __tests__/Rank.test.js
@@ -0,0 +1,15 @@
+import * as jasmineCheck from 'jasmine-check';
+
+import '../resources/jest.d.ts';
+
+import { Rank } from '../src/lib/Rank';
+
+jasmineCheck.install();
+
+
+describe('Rank', () => {
+    it('Should sort an array of objects by property value', () => {
+
+    })
+})
+
