cdf91df6ef092a29a7c8ecb4da5f176f15a2cac7
Anne Bione <annebione@gmail.com>
2018-08-31 14:05:27 -0300

    Capitalize function done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Rank from './lib/Rank'
 import ReverseRank from './lib/ReverseRank'
 import SetRanking from './lib/SetRanking'
 import Render from './lib/Render';
+import CapitalizeAll from './lib/CapitalizeAlll';
 
 export var Soul = {
   Rank: Rank,
   ReverseRank: ReverseRank,
   SetRanking: SetRanking,
-  Render: Render
+  Render: Render,
+  CapitalizeAll: CapitalizeAll
 } 
